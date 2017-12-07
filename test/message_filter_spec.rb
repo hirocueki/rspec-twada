0bcdf0d4d5236c469080e0f936886bba8c6d6008
hirocueki <hirocueki@gmail.com>
2017-12-07 17:05:27 +0900

    use beore

--- test/message_filter_spec.rb
+++ test/message_filter_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 require 'message_filter'
 
 RSpec.describe MessageFilter do
+    before(:each) do
+        @filter = MessageFilter.new('foo')
+    end
     it 'should detect message with NG word' do
-        filter = MessageFilter.new('foo')
-        expect(filter.detect?('hello from foo')).to be true
+        # filter = MessageFilter.new('foo')
+        expect(@filter.detect?('hello from foo')).to be true
     end
     it 'should not detect message without NG word' do
-        filter = MessageFilter.new('foo')
-        expect(filter.detect?('hello world!')).to be false
+        # filter = MessageFilter.new('foo')
+        expect(@filter.detect?('hello world!')).to be false
     end
 end
