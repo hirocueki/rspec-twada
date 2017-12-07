bd3b0fe95a6d107ec1e38c4dd07daacc5b383313
hirocueki <hirocueki@gmail.com>
2017-12-07 17:01:21 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rspec-twada
+RSpecをやっていく。
+
+- [RSpec の入門とその一歩先へ - t-wadaの日記](http://d.hatena.ne.jp/t-wada/20100228/p1)
---
+++ test/message_filter_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'message_filter'
+
+RSpec.describe MessageFilter do
+    it 'should detect message with NG word' do
+        filter = MessageFilter.new('foo')
+        expect(filter.detect?('hello from foo')).to be true
+    end
+    it 'should not detect message without NG word' do
+        filter = MessageFilter.new('foo')
+        expect(filter.detect?('hello world!')).to be false
+    end
+end
